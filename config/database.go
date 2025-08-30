202aaded607f6a12ead89e68bc4fccd7ee25f2d6
NightInvader <muammarsadam13@gmail.com>
2025-08-31 00:50:05 +0700

    try 2

--- config/database.go
+++ config/database.go
@@ -1,10 +1,11 @@
 package config
 
 import (
-	//"github.com/joho/godotenv"
 	"fmt"
 	"log"
 	"os"
+
+	"github.com/joho/godotenv"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -16,10 +17,23 @@ var (
 )
 
 func ConnectDB() {
-	psqlinfo := os.Getenv("DATABASE_PUBLIC_URL")
-	if psqlinfo == "" {
-		log.Fatal("DATABASE_URL is not set")
+
+	err = godotenv.Load(".env")
+	if err != nil {
+		panic("Error loading .env file")
 	}
+
+	psqlinfo := fmt.Sprintf(`host=%s port=%s user=%s password=%s dbname=%s sslmode=disable`,
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+	// psqlinfo := os.Getenv("DATABASE_PUBLIC_URL")
+	// if psqlinfo == "" {
+	// 	log.Fatal("DATABASE_URL is not set")
+	// }
 
 	database, err := gorm.Open(postgres.Open(psqlinfo), &gorm.Config{})
 	if err != nil {
